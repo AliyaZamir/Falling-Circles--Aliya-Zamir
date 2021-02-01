c4b338984a56c1b262f9cbcc35535c045f8a3740
AliyaZamir <69330907+AliyaZamir@users.noreply.github.com>
2021-02-01 18:50:04 -0500

    Update candies.js

--- candies.js
+++ candies.js
@@ -6,12 +6,16 @@ class Candies{
         this.diameter = d;
         this.candy=Matter.Bodies.circle(x,y,d, {isStatic: false});
         World.add(myWorld,this.candy);
+        this.image = loadImage("images/redCandy.png");
     }
 
     display(){
-        fill("green");
+       // fill("green");
         var pos = this.candy.position;
-        circle(pos.x,pos.y,this.diameter);
+        candy1 = createSprite(pos.x,pos.y, 10, 10);
+        group.add(candy1);
+        image (this.image, pos.x,pos.y);
+        //circle(pos.x,pos.y,this.diameter);
         //console.log(this.candy);
     }
 }
