4d4cdad841d13f1e4fbb8087d48cd1661502b26f
AliyaZamir <69330907+AliyaZamir@users.noreply.github.com>
2021-02-01 18:50:49 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,8 +8,18 @@ const Constraint = Matter.Constraint;
 var pc, edges;
 var myEngine, myWorld;
 var candyArray = [];
+var sky;
+var destroy;
+var score = 0;
+var group;
+var candy1;
 
 function preload(){
+
+sky = loadImage("images/blueSky.png");
+//load image for PC
+
+//candy1 = loadImage("images/candyClipart.jpg");
 
 }
 
@@ -21,41 +31,52 @@ function setup(){
  
  pc = createSprite(displayWidth/2,displayHeight-250,20,20);
  edges = createEdgeSprites();
- console.log(edges);
+ //console.log(edges);
+
+ //create a new group
+ group = new Group();
  
 
 }
 
 function draw(){
-    background('white');
+    background(sky);
     
     Engine.update(myEngine);
+    stroke ("black");
+    textSize(40);
+    text('Score: ' + score , displayWidth - 275, 50);
 
-    //if (){
+    //Calculate score
+    if (pc.isTouching(group)){
+        score += 5;
 
-   // }
+    }
 
- /*  if (pc.isTouching(edges[2])){
+
+   if (pc.isTouching(edges[2])){
     pc.velocityY = 0;
    }
 
    if (pc.isTouching(edges[0]) || pc.isTouching(edges[1])){
        
     pc.velocityX = 0;
-   }*/
+   }
 
-  
+
   if(frameCount % 60 === 0){
     c1 = dropCandies();
     candyArray.push(c1);
-    for(var i=0; i < candyArray.length; i++){
-        console.log(candyArray[i]);
-        candyArray[i].display(); 
-    }
     
+    
+      
     
   }
   
+  for(var i=0; i < candyArray.length; i++){
+   // console.log("In for.." + i + "--> " + candyArray[i]);
+    candyArray[i].display(); 
+  }
   
   
 drawSprites();
@@ -84,8 +105,9 @@ function keyPressed(){
 function dropCandies(){
 
 //var candy = new Candies(Math.round(random(20, displayWidth - 20), 0,30)); 
-var candy = new Candies(30, 30, 30);
-console.log(candy);
+//console.log(random(30, displayWidth - 30));
+var candy = new Candies(random(30, displayWidth - 30), 30, 30);
+//console.log(candy);
 return candy;
 
 }
